1c7f1ca4c45716a394be705dc057e5de68082fd9
TechieTeee <techieteee@protonmail.com>
2023-08-25 21:42:27 +0000

    Updated ETL

--- main.py
+++ main.py
@@ -16,35 +16,39 @@ def main():
     config = builder.build()
     client = PolywrapClient(config)
 
-    # Read the data from the CSV file
-    with open("ETH-USD.csv", "r") as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip the header row
-        data = list(reader)
-
-    # Create a pandas dataframe from the data
-    df = pd.DataFrame(data, columns=["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"])
-
-    # Convert the Date column to datetime with the appropriate format
-    df["Date"] = pd.to_datetime(df["Date"], format='%Y-%m-%d')
-
-    # Calculate the moving average for each price column
-    for column in ["Open", "High", "Low", "Close"]:
-        df["MA_" + column] = df[column].rolling(window=10).mean()
-
-    # Convert the DataFrame to JSON format
-    data_json = df.to_json()
-
-    # Use the Polywrap client to invoke the wrapper to process the data
+    # Use the Polywrap client to invoke the IPFS wrapper to retrieve data
     uri = Uri.from_str('wrapscan.io/polywrap/ipfs-http-client')  # Updated URI
     args = {
-        "cid": "QmZ4d7KWCtH3xfWFwcdRXEkjZJdYNwonrCwUckGF1gRAH9",
+        "cid": "QmW54fYzbsYbgbu2rhShauWEKgjjkS1keecTncp24L4DLu",  # Uploaded CSV file's CID with ETH USD data
         "ipfsProvider": "https://ipfs.io",
     }
     result = client.invoke(uri=uri, method="cat", args=args, encode_result=False)
 
-    # Print the results
-    print(result)
+    # Decode the retrieved data from bytes to string
+    retrieved_data = result.decode('utf-8')
+
+    # Convert the retrieved data to a DataFrame
+    df = pd.read_csv(pd.compat.StringIO(retrieved_data))
+
+    # Perform necessary transformations and calculations on the DataFrame
+    # Calculate moving averages for relevant columns
+    for column in ["Open", "High", "Low", "Close"]:
+        df["MA_" + column] = df[column].rolling(window=10).mean()
+
+    # Convert the DataFrame to JSON format
+    processed_data_json = df.to_json()
+
+    # Use the IPFS wrapper to upload the processed data to IPFS
+    ipfs_uri = Uri.from_str('wrapscan.io/polywrap/ipfs-http-client')  # IPFS wrapper URI
+    ipfs_args = {
+        "data": processed_data_json,
+        "ipfsProvider": "https://ipfs.io",
+    }
+    ipfs_result = client.invoke(uri=ipfs_uri, method="add", args=ipfs_args, encode_result=False)
+
+    # Print the IPFS hash of the uploaded data
+    print("Processed data uploaded to IPFS with hash:", ipfs_result.decode('utf-8'))
 
 if __name__ == "__main__":
     main()
+
